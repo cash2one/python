a0f0ad5a9b39dda330388f4898e220d596cb4264
yangmingsong <13002023939@163.com>
2015-09-28 17:34:06 +0800

    20150928

--- luxury_project/luxuryBrand/brandList.py
+++ luxury_project/luxuryBrand/brandList.py
@@ -3,16 +3,19 @@ __author__ = '613108'
 import sys
 from spiderFrame.downLoad.DownLoader import DownLoader
 from pyquery import PyQuery as pq
+
 reload(sys)
+# noinspection PyUnresolvedReferences
 sys.setdefaultencoding('utf8')
+
 
 def brandList():
     DL = DownLoader('http://www.xiu.com/brand.html')
     src = DL.selenium()
-    d=pq(src)
-    text=d.find('li>dl>dd>a').my_text()
-    res=[]
+    d = pq(src)
+    text = d.find('li>dl>dd>a').my_text()
+    res = []
     for item in text:
         res.extend(item.split('/'))
-    res=map(lambda x:x.replace(' ',''),res)
+    res = map(lambda x: x.replace(' ', ''), res)
     return res
