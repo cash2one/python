648581a28c76e6236dbdce11ba476eadbc919fbd
yangmingsong <13002023939@163.com>
2015-09-30 00:53:29 +0800

    20150930

---
+++ spiderFrame/ProxySupporter-DEMO.py
@@ -0,0 +1,6 @@
+#coding:utf8
+__author__ = 'Administrator'
+from ProxySupporter import ProxySupporter
+
+test=ProxySupporter()
+test.gather()
--- spiderFrame/ProxySupporter.py
+++ spiderFrame/ProxySupporter.py
@@ -3,17 +3,40 @@ __author__ = '613108'
 """
 提供代理抓取、检测、持久化及代理返回等等功能
 """
+from spiderFrame.downLoad.DownLoader import DownLoader
+from spiderFrame.downLoad import DownLoad_base_noFollow as DBN
+from pyquery import PyQuery as pq
+
+class DLer(DBN.DownLoadBase):
+    def __init__(self):
+        DBN.DownLoadBase.__init__(self)
+
+    # override
+    def startUrlList(self):
+        DL=DownLoader('http://www.blackhatworld.com/blackhat-seo/f103-proxy-lists/')
+        src=DL.highOrderUrllib2()
+        d=pq(src)
+        topicLink=d.find('.inner .threadtitle a')
+        startUrl=[pq(item).attr('href') for item in topicLink[2:]]
+        return startUrl
 
 class ProxySupporter:
     def __init__(self):
-        self.__init__()
+        pass
 
     def gather(self):
-        """
-        代理收集
-        :return:
-        """
-        pass
+        import time
+        downloader=DLer()
+        downloader.downLoad(100)
+        i=0
+        while i>10:
+            print(i)
+            if not DBN.queueForDownLoad.empty():
+                src=DBN.queueForDownLoad.get(timeout=10)
+                print(src)
+            else:
+                time.sleep(20)
+                i+=1
 
     def test(self):
         """
