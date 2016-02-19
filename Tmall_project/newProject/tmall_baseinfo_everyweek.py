507f43021e1417ee564f86bb714ca473359bb070
yangmingsong <13002023939@163.com>
2016-02-19 19:09:52 +0800

    20160219_com

--- Tmall_project/newProject/tmall_baseinfo_everyweek.py
+++ Tmall_project/newProject/tmall_baseinfo_everyweek.py
@@ -4,17 +4,38 @@
 # This script is used to unique tmall data(crawl weekly),and store date to mysql_db ;
 # before using , you should modify crawled_date in cofing text .
 
-from dataAnalysis import tmallAnalysis
 from ms_spider_fw.DBSerivce import DBService
 import datetime
+import os
+import pandas
+import numpy as np
 
 # config text:
-spider_time = datetime.date(2016, 1, 18)  # crawled_date should be modified before use
+# crawled_date should be modified before use
+spider_time = datetime.date(2016, 2, 19)
 spider_week = spider_time.strftime('%U')
 
 
+# noinspection PyUnresolvedReferences
+def temp_data(path):
+    fileListLen = len(os.listdir(path))
+    names = globals()
+    count = 1
+    for item in os.listdir(path):
+        fileName = path + '\\' + item
+        print(u'正在处理第 %s 个文件，总共 %s 个文件。' % (count, fileListLen))
+        names['data' + '_' + str(count)] = pandas.read_csv(fileName)
+        count += 1
+    name_space = []
+    for i in range(1, fileListLen + 1):
+        name_space.append(names['data' + '_' + str(i)])
+    data_t = pandas.concat(name_space)
+    data = data_t.drop_duplicates(['href']).values.tolist()
+    return [map(lambda x: '' if x is np.nan else x, t) for t in data], data_t.columns.tolist()
+
+
 def getData(path):
-    data, titleNeedToHandle = tmallAnalysis.getDistinctShopListDir(path=path)
+    data, titleNeedToHandle = temp_data(path)
     indexToHandleData = [0, 1, 14, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 17, 19, 20, 22, 23, 25, 26]
     week = spider_week
     dataOk = []
@@ -24,7 +45,6 @@ def getData(path):
             temp.append(item[itemIndex])
         temp.extend([week, spider_time])
         dataOk.append(temp)
-
     return dataOk
 
 
